8ccb94f2e70c4204f98897f07430f94ad71387de
Andrii Tkachuk <kovklad@gmail.com>
2025-04-19 12:45:05 +0200

    add fixed next.config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['i.kym-cdn.com'],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "i.kym-cdn.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "i.imgur.com",
-        pathname: "/**",
-      },
-    ],
+  output: process.env.RAILWAY_ENVIRONMENT ? 'standalone' : 'export',
+  experimental: {
+    turbo: false // Disable Turbopack on Railway
   },
+  images: {
+    unoptimized: true // Disable image optimization if using external URLs
+  },
+  // images: {
+  //   domains: ['i.kym-cdn.com'],
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "i.kym-cdn.com",
+  //       pathname: "/**",
+  //     },
+  //     {
+  //       protocol: "https",
+  //       hostname: "i.imgur.com",
+  //       pathname: "/**",
+  //     },
+  //   ],
+  // },
 }
 
 module.exports = nextConfig
