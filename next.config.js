d90aa04fffd6d49b4acda25733a021308dc5a680
Andrii Tkachuk <kovklad@gmail.com>
2025-04-19 12:53:23 +0200

    add previous version

--- next.config.js
+++ next.config.js
@@ -1,28 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: process.env.RAILWAY_ENVIRONMENT ? 'standalone' : 'export',
-  experimental: {
-    turbo: false // Disable Turbopack on Railway
-  },
   images: {
-    unoptimized: true // Disable image optimization if using external URLs
+    // domains: ['i.kym-cdn.com'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.kym-cdn.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+        pathname: "/**",
+      },
+    ],
   },
-  // images: {
-  //   domains: ['i.kym-cdn.com'],
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "i.kym-cdn.com",
-  //       pathname: "/**",
-  //     },
-  //     {
-  //       protocol: "https",
-  //       hostname: "i.imgur.com",
-  //       pathname: "/**",
-  //     },
-  //   ],
-  // },
 }
 
 module.exports = nextConfig
